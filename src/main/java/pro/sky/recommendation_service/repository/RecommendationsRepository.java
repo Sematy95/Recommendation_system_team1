259410dd8819438744ee629708462032f84095dd
tychk <semarooney@yandex.ru>
2025-01-26 23:34:34 +0300

    feat-minor updates

--- src/main/java/pro/sky/recommendation_service/repository/RecommendationsRepository.java
+++ src/main/java/pro/sky/recommendation_service/repository/RecommendationsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import pro.sky.recommendation_service.domain.Condition;
 import pro.sky.recommendation_service.domain.Transaction;
 
 import java.util.List;
@@ -15,6 +16,10 @@ public class RecommendationsRepository {
 
     public RecommendationsRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private String CreateSqlFromCondition(Condition condition) {
+        return "sql";
     }
 
     public List<Transaction> getTransactions(UUID user_ID) {
