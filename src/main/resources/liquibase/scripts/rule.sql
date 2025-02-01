8ea324e53d62aebb8a834a639f0c63b6a5acfd21
tychk <semarooney@yandex.ru>
2025-02-01 20:52:02 +0300

    fix-dynamic rule with invest500 was corrected

--- src/main/resources/liquibase/scripts/rule.sql
+++ src/main/resources/liquibase/scripts/rule.sql
@@ -55,7 +55,7 @@ VALUES (1, 'Invest 500', '147f6a0f-3b91-413b-ab99-87f081d60d5a',
 -- changeset sematy:3
 INSERT INTO condition (id, query, product_Type, transaction_Name, compare_Type, compare_Value, negate,parallel_condition_id) VALUES
 (1, 0, 0, NULL, NULL, NULL, FALSE,NULL),
-(2, 0, 1, NULL, NULL, NULL, TRUE,null),
+(2, 0, 2, NULL, NULL, NULL, TRUE,null),
 (3, 2, 3, 1, 0, 1000, FALSE,NULL),
 
 
