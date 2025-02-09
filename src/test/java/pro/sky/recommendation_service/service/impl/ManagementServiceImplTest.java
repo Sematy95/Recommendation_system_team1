966f0c2d100e617c64a7d121c56a3a3a0a0d786f
Moisuk-Dmitri <moisuk.d@gmail.com>
2025-02-09 14:24:27 +0300

    added tests for clear caches method

--- src/test/java/pro/sky/recommendation_service/service/impl/ManagementServiceImplTest.java
+++ src/test/java/pro/sky/recommendation_service/service/impl/ManagementServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.Cache;
@@ -18,6 +19,7 @@ import pro.sky.recommendation_service.service.ManagementService;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,7 +40,15 @@ class ManagementServiceImplTest {
 
 
     @Test
-    void clearCaches() {}
+    void clearCaches() {
+        Cache cache = Mockito.mock(Cache.class);
+        when(caffeineCacheManager.getCacheNames()).thenReturn(List.of("cacheName1", "cacheName2"));
+        Mockito.when(caffeineCacheManager.getCache(anyString())).thenReturn(cache);
+
+        managementService.clearCaches();
+
+        assertFalse(cache.invalidate());
+    }
 
     @Test
     @DisplayName("Вывод информации о имени и версии сервиса")
